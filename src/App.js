be59eabdc058182677a2017341dfebe14e58f389
Carlos Plaza <carlosfplaza@hotmail.com>
2020-11-03 11:38:37 -0300

    counter completo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import PropTypes from 'prop-types';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function SimpleCounter(props) {
+  return (<div className="main-container">
+      <div className="clock">
+          <i className="far fa-clock"></i>
+      </div>
+      <div className="fourthDigit">{props.fourth % 10}</div>
+      <div className="thirdDigit">{props.third % 10}</div>
+      <div className="secondDigit">{props.second % 10}</div>
+      <div className="firstDigit">{props.first % 10}</div>
+  </div>)
 }
 
-export default App;
+SimpleCounter.propTypes = {
+  fourth: PropTypes.number,
+  third: PropTypes.number,
+  second: PropTypes.number,
+  first: PropTypes.number
+};
+
+
+
+
+export default SimpleCounter;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SimpleCounter from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let counter = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+setInterval(function(){
+  const fourth = Math.floor(counter/1000);
+  const third = Math.floor(counter/100);
+  const second = Math.floor(counter/10);
+  const first = Math.floor(counter/1);
+  console.log(fourth,third,second,first)
+  counter ++;
+    ReactDOM.render(
+      <React.StrictMode>
+        <SimpleCounter first={first} second={second} third={third} fourth={fourth}/>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+},1000);
+
+
+
 reportWebVitals();
